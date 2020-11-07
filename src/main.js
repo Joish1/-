f98adfeeacbdb525bd2ef3c632bb48b9f7da7c19
Joish <1270685166@qq.com>
2020-11-07 14:57:31 +0800

    生产者消费者模型

---
+++ src/main.js
@@ -0,0 +1,52 @@
+let productLock = false
+let maxLength = 10
+let arrayLength = 0
+
+let $producer = document.querySelector('.producer')
+let $consumer = document.querySelector('.consumer')
+let $produceSign = document.querySelector('.produceSign')
+
+let producer = () => {
+    setTimeout(() => {
+        if (!productLock) {
+            productLock = true
+            if (arrayLength < maxLength) {
+                arrayLength++
+            } else {
+                alert('仓库已满，无法生产。')
+            }
+            $produceSign.innerHTML = '生产完成，' + '目前仓库中已有产品：' + arrayLength + '个'
+            productLock = false
+        } else {
+            alert('生产者或消费者正在工作中，请等一等...')
+        }       
+    }, 500)
+}
+
+let consumer = () => {
+    setTimeout(() => {
+        if (!productLock) {
+            productLock = true
+            if (arrayLength) {
+                arrayLength--
+            } else {
+                alert('仓库是空的，请等待生产者生产。')
+            }
+            $produceSign.innerHTML = '消费完成，' + '目前仓库中已有产品：' + arrayLength + '个'
+            productLock = false
+        } else {
+            alert('生产者或消费者正在工作中，请等一等...')
+        }       
+    }, 500)
+}
+
+$producer.onclick = () => {
+    producer()
+    $produceSign.innerHTML = '生产中...'
+}
+
+$consumer.onclick = () => {
+    consumer()
+    $produceSign.innerHTML = '消费中...'
+}
+
